b34a1f42287dfc657fe399b126ca242de5b08212
Adithyakb2005 <adithyakb1001@gmail.com>
2024-06-24 11:22:46 +0530

    new file add

---
+++ nested.py
@@ -0,0 +1,15 @@
+"""for i in range(4):
+    for i in range(3):
+        print("*",end="\t")
+    print()"""
+"""a=1
+for i in range(3):
+    for j in range(3):
+        print(a,end="\t")
+        a+=1
+    print()"""
+
+for i in range(1,4):
+    for j in range(i):
+        print(j,end="\t")
+    print()
