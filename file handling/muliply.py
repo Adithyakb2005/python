ea49d415abfc94a7d73cdcd5d52b5cad13417a7c
Adithyakb2005 <adithyakb1001@gmail.com>
2024-07-10 11:10:40 +0530

    new file add

--- file handling/muliply.py
+++ file handling/muliply.py
@@ -4,9 +4,10 @@
 #     f.write(f"{a} x {i} = {a*i}\n")
 
 a=int(input("enter the number"))
-f=open('python/file handling/multiple','w')
+f=open('python/file handling/multiple1','w')
 for i in range(1,a+1):
     f.write(f"multiplication table {i}:\n")
     for j in range(1,11):
         f.write(f"{j} x {i} = {a*j}\n")
     f.write("\n")
+f.close()
